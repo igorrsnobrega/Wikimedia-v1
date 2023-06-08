be8518efb6339bc16aef73426d0483767f79952d
Igor Nobrega <igor.nobrega1@hotmail.com>
2023-06-08 13:55:50 -0300

    add: criando modulo producer

---
+++ kafka-producer-wikimedia/src/main/java/com/example/wikimedia/WikimediaProducerApplication.java
@@ -0,0 +1,11 @@
+package com.example.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaProducerApplication.class);
+    }
+}
